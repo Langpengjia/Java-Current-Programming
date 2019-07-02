a5555f734a9f0105c6e8e8d7b0cafc10541c0637
Desire <562457731@qq.com>
2019-07-02 15:14:03 +0800

    add gitignore

--- src/phase1/chapter5/BooleanLock.java
+++ src/phase1/chapter5/BooleanLock.java
@@ -1,8 +1,12 @@
 package phase1.chapter5;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+
+import static java.lang.System.currentTimeMillis;
+import static java.lang.Thread.currentThread;
 
 /**
  * @classname: BooleanLock
@@ -16,20 +20,21 @@ public class BooleanLock implements Lock {
 
     private boolean locked = false;
 
-    private final List<Thread> blockedLsit = new ArrayList<>();
+    private final List<Thread> blockedList = new ArrayList<>();
 
 
     @Override
     public void lock() throws InterruptedException {
 
-        synchronized (this){
-            while (locked){
-                blockedLsit.add(Thread.currentThread());
+        synchronized (this) {
+            while (locked) {
+                //此处与原书有歧义，原书无法通过编译 currentThread()
+                blockedList.add(currentThread());
                 this.wait();
             }
-            blockedLsit.remove(Thread.currentThread());
+            blockedList.remove(currentThread());
             this.locked = true;
-            this.currentThread = Thread.currentThread();
+            this.currentThread = currentThread();
         }
 
     }
@@ -37,15 +42,41 @@ public class BooleanLock implements Lock {
     @Override
     public void lock(long mills) throws InterruptedException, TimeoutException {
 
+        synchronized (this) {
+            if (mills <= 0) {
+                this.lock();
+            } else {
+                long remainingMills = mills;
+                //此处与原书不符
+                long endMills = currentTimeMillis() + remainingMills;
+                while (locked) {
+                    if (remainingMills <= 0) {
+                        throw new TimeoutException("can not get the lock during " + mills);
+                    }
+                    if (!blockedList.contains(currentThread())) {
+                        blockedList.add(currentThread());
+                    }
+                    this.wait(remainingMills);
+                    remainingMills = endMills - currentTimeMillis();
+                }
+            }
+        }
     }
 
     @Override
     public void unlock() {
 
+        synchronized (this) {
+            if (currentThread == currentThread()) {
+                this.locked = false;
+                this.notifyAll();
+            }
+        }
     }
 
     @Override
     public List<Thread> getBlockedThread() {
-        return null;
+        //不可变list符合面向对象编程思想
+        return Collections.unmodifiableList(blockedList);
     }
 }
